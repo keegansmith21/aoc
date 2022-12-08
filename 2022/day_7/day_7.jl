b97fc2ad2006896b7d0683c8a43a9a2e6baa1957
keegansmith21 <keegan.r.s21@gmail.com>
2022-12-08 10:28:55 +0800

    day 7

--- 2022/day_7/day_7.jl
+++ 2022/day_7/day_7.jl
@@ -2,12 +2,12 @@ using FilePaths
 
 function main(in_file)
     open(in_file, "r") do f
-        directories = disk_usage(readlines(f))
-        total = sum_of_dirs(directories)
-        println("Sum of file usage where files <= 100000: ", total)
-        free_space = 7e7 - directories["/"]
+        directories, total_usage = disk_usage(readlines(f))
+        total_under = sum_of_dirs(directories)
+        println("Sum of file usage where files <= 100000: ", total_under)
+        free_space = 7 * 10^7 - total_usage
         println("Current free space: ", free_space)
-        rem_dir_space = 3e7 - free_space
+        rem_dir_space = 3 * 10^7 - free_space
         println("Find directory with size of at least ", rem_dir_space)
         size, name = closest_dir_over(directories, rem_dir_space)
         println("Delete the directory:", name, " which will free up ", size, " bytes")
@@ -18,23 +18,23 @@ end
 function disk_usage(lines)
     directories = Dict{String,Int}()
     cwd = ""
+    total_usage = 0
     for l in lines
         parts = split(l)
         if parts[1] == "\$" && parts[2] == "cd"
             if parts[3] == ".."
                 cwd = dirname(cwd)
-                println(cwd)
             else
                 cwd = joinpath(cwd, parts[3])
                 directories[cwd] = 0
-                println(cwd)
             end
         elseif tryparse(Int, parts[1]) !== nothing
-            usage = parse(Int, split(l, " ")[1])
+            usage = parse(Int, parts[1])
+            total_usage += usage
             add_size_to_dirs(cwd, directories, usage)
         end
     end
-    return directories
+    return directories, total_usage
 end
 
 
@@ -63,7 +63,7 @@ end
 
 
 function closest_dir_over(dirs, minimum)
-    chosen_size = 1e10
+    chosen_size = 1 * 10^10
     chosen_name = ""
     for (name, size) in dirs
         if size > minimum && size < chosen_size
@@ -72,6 +72,7 @@ function closest_dir_over(dirs, minimum)
         end
     end
     return chosen_size, chosen_name
-
 end
+
+
 main("input.txt")
