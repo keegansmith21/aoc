c1688f52ab096b8c60db852a642cc1ac9b87ff8f
keegansmith21 <keegan.r.s21@gmail.com>
2022-12-20 18:06:07 +0800

    day 11

--- 2022/day_11/day_11.jl
+++ 2022/day_11/day_11.jl
@@ -2,12 +2,10 @@ OPERAND_MAP = Dict("+" => +, "-" => -, "/" => /, "*" => *, "^" => ^)
 
 function main(in_file)
     monkeys = parse_input(in_file)
-    for m in keys(monkeys)
-        println(monkeys[m]["items"])
-    end
-    # round_inspections = simulate_rounds(monkeys, 20, 3)
-    # println("Part 1 results:")
-    # calc_results(round_inspections, monkeys)
+    round_inspections = simulate_rounds(monkeys, 20, 3)
+    println("Part 1 results:")
+    monkeys = parse_input(in_file)
+    calc_results(round_inspections, monkeys)
     round_inspections = simulate_rounds(monkeys, 10000, 1)
     println("Part 2 results:")
     calc_results(round_inspections, monkeys)
@@ -24,10 +22,10 @@ function parse_input(in_file)
     monkey_no = nothing
     for line in lines
         if occursin("Monkey", line)
-            monkey_no = parse(Int128, split(line, ":")[1][end])
+            monkey_no = parse(Int, split(line, ":")[1][end])
             monkeys[monkey_no] = Dict{String,Any}()
         elseif occursin("Starting items:", line)
-            monkeys[monkey_no]["items"] = [parse(BigInt, i) for i in split(split(line, "Starting items:")[end], ",")]
+            monkeys[monkey_no]["items"] = [parse(Int, i) for i in split(split(line, "Starting items:")[end], ",")]
         elseif occursin("Operation:", line)
             operation = split(line, "new = old ")[end]
             operand = split(operation, " ")[1]
@@ -43,11 +41,11 @@ function parse_input(in_file)
             monkeys[monkey_no]["operation"]["value"] = val
         elseif occursin("Test:", line)
             monkeys[monkey_no]["test"] = Dict()
-            monkeys[monkey_no]["test"]["condition"] = parse(Int128, split(line, " ")[end])
+            monkeys[monkey_no]["test"]["condition"] = parse(Int, split(line, " ")[end])
         elseif occursin("true: throw to monkey", line)
-            monkeys[monkey_no]["test"]["true"] = parse(Int128, split(line, " ")[end])
+            monkeys[monkey_no]["test"]["true"] = parse(Int, split(line, " ")[end])
         elseif occursin("false: throw to monkey", line)
-            monkeys[monkey_no]["test"]["false"] = parse(Int128, split(line, " ")[end])
+            monkeys[monkey_no]["test"]["false"] = parse(Int, split(line, " ")[end])
         end
     end
     return monkeys
@@ -55,9 +53,8 @@ end
 
 
 function simulate_rounds(monkeys, n_rounds, worry_divisor)
-    round_inspections = Vector{BigInt}[]
+    round_inspections = Vector{Int}[]
     for r in 1:n_rounds
-        println(r)
         monkeys, inspections = simulate_round(monkeys, worry_divisor)
         push!(round_inspections, inspections)
     end
@@ -66,20 +63,19 @@ end
 
 
 function simulate_round(monkeys, worry_divisor)
-    inspections = fill(BigInt(0), length(keys(monkeys)))
+    inspections = fill(Int(0), length(keys(monkeys)))
+    supermod = prod([monkeys[i]["test"]["condition"] for i in keys(monkeys)])
     for monkey_no in sort(collect(keys(monkeys)))
-        #println("Monkey Number $(monkey_no)")
         for _ in eachindex(monkeys[monkey_no]["items"])
             # Monkey inspects the next item on the list
             monkeys[monkey_no]["items"][1] = monkeys[monkey_no]["operation"]["operand"](monkeys[monkey_no]["items"][1], monkeys[monkey_no]["operation"]["value"])
-            #println("Item worry: $(monkeys[monkey_no]["items"][1])")
             inspections[monkey_no+1] += 1
 
             # Item isn't damaged, divide worry by divisor
+            monkeys[monkey_no]["items"][1] = mod(monkeys[monkey_no]["items"][1], supermod) # Part 2 adjustment
             monkeys[monkey_no]["items"][1] ÷= worry_divisor
 
             # Monkey tests you to see where to throw the item
-            #println("Test condition: $(monkeys[monkey_no]["test"]["condition"])")
             if mod(monkeys[monkey_no]["items"][1], monkeys[monkey_no]["test"]["condition"]) == 0
                 # Throw to monkey in 'true' condition
                 recipient = monkeys[monkey_no]["test"]["true"]
@@ -88,7 +84,6 @@ function simulate_round(monkeys, worry_divisor)
             end
 
             # Throw the item
-            #println("Throws item of worry level $(monkeys[monkey_no]["items"][1]) to monkey $(recipient)")
             push!(monkeys[recipient]["items"], popfirst!(monkeys[monkey_no]["items"]))
 
         end
@@ -98,6 +93,7 @@ end
 
 
 function calc_results(round_inspections, monkeys)
+    println("Final Items:")
     for m in eachindex(collect(keys(monkeys)))
         println(monkeys[m-1]["items"])
     end
@@ -111,5 +107,5 @@ function calc_results(round_inspections, monkeys)
     println("The level of monkey business is $(sort(summed_inspections)[end] * sort(summed_inspections)[end-1])")
 end
 
-main("test_input.txt") # Answer should be 10605
-#main("input.txt")
+main("test_input.txt") # Answer should be 10605/2713310158
+main("input.txt")
