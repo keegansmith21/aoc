b0b16041d7dc48a7cfeb756c6373dfd0e3d6d3f4
keegansmith21 <keegansmith21@github.com>
2023-12-07 08:53:51 +0800

    made basic template

---
+++ template.py
@@ -0,0 +1,25 @@
+def fmt(input):
+    pass
+
+
+def main_1(input):
+    fmt(input)
+
+    answer = None
+    print(f"PT 1 ANSWER: {answer}")
+    pass
+
+
+def main_2(input):
+    fmt(input)
+
+    answer = None
+    print(f"PT 2 ANSWER: {answer}")
+    pass
+
+
+if __name__ == "__main__":
+    with open("202X/day_X/input.txt", "r") as f:
+        input = f.readlines()
+    # main_1(input)
+    # main_2(input)
